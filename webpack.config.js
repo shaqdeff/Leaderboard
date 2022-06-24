3d5bd5cdcd731ef69c3cf96bd0591ecb2abaa559
Shaquille Ndunda <92020713+shaqdeff@users.noreply.github.com>
2022-06-24 12:35:10 +0300

    Wireframe Milestone (#1)
    * Create wireframe structure
    
    * Modify build script
    
    * Add styles to wireframe
    
    * Update README
    
    * Install gh-pages deploy
    
    * Install gh-pages
    
    * Fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    main: path.resolve(__dirname, './src/index.js')
+    main: path.resolve(__dirname, './src/index.js'),
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
     clean: true,
+    publicPath: '/Leaderboard/',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
   module: {
@@ -30,4 +31,4 @@ module.exports = {
   optimization: {
     runtimeChunk: 'single',
   },
-}
+};
